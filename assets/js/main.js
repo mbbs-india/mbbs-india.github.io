d4aeb16b268c165cb9530d8780eb3d62388be07f
Aziza <kubatbek_k@auca.kg>
2017-03-03 23:00:48 +0600

    auca full content prototype

--- assets/js/main.js
+++ assets/js/main.js
@@ -408,6 +408,8 @@
 
 })(jQuery);
 
+
+// following is custom code
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -425,4 +427,22 @@ window.onclick = function(event) {
       }
     }
   }
+};
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+		/* Toggle between adding and removing the "active" class,
+		 to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+		/* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
 }
